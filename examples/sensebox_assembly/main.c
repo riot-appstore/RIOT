67508ca35c7eb7952e2860a7c0b9befaf4a27e38
danpetry <daniel.petry@fu-berlin.de>
2018-12-13 19:03:51 +0100

    Implement basic testing and config. Needs debugging

--- examples/sensebox_assembly/main.c
+++ examples/sensebox_assembly/main.c
@@ -23,19 +23,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "shell.h"
 #include "periph/gpio.h"
 #include "periph/adc.h"
+#include "periph/rtc.h"
+
+#include "shell.h"
 #include "xtimer.h"
 #include "msg.h"
 #include "thread.h"
 #include "fmt.h"
- #include "periph/rtc.h"
- #include "net/loramac.h"
+#include "net/loramac.h"
+
 #include "semtech_loramac.h"
 #include "lora_serialization.h"
+
 #include "hdc1000.h"
 #include "hdc1000_params.h"
+#include "tsl4531x.h"
+#include "tsl4531x_params.h"
+#include "ds18.h"
+#include "ds18_params.h"
 
 #define RES             ADC_RES_10BIT
 // Valves
@@ -44,20 +51,64 @@
 ////        gpio_clear(GPIO_PIN(PA, 5));
 ////        gpio_clear(GPIO_PIN(PA, 7));
 
-/* Messages are sent every 5min to respect the duty cycle on each channel */
-#define PERIOD              (300U)
- #define SENDER_PRIO         (THREAD_PRIORITY_MAIN - 1)
+#define SENDER_PRIO         (THREAD_PRIORITY_MAIN - 1)
+
 static kernel_pid_t sender_pid;
 static char sender_stack[THREAD_STACKSIZE_MAIN / 2];
- semtech_loramac_t loramac;
+semtech_loramac_t loramac;
 lora_serialization_t serialization;
- static uint8_t deveui[LORAMAC_DEVEUI_LEN];
+
+static uint8_t deveui[LORAMAC_DEVEUI_LEN];
 static uint8_t appeui[LORAMAC_APPEUI_LEN];
 static uint8_t appkey[LORAMAC_APPKEY_LEN];
 
-/* Sensor device descriptors */
+uint16_t plant_1_watering_level;
+uint16_t plant_2_watering_level;
 
+/*********** Configurations ******************/
+//#define VALVE_WATERING_ON
+#define HARDWARE_TEST_ON
+//#define LORA_DATA_SEND_ON
+
+#define PERIOD              (300U)   /* messages sent every 5 mins */
+
+#define MOISTURE_SENSOR_1_PIN      ADC_LINE(0)
+#define MOISTURE_SENSOR_2_PIN      ADC_LINE(2)
+#define MOISTURE_SENSOR_3_PIN      ADC_LINE(5)
+
+#define VALVE_CONTROL_1_PIN        GPIO_PIN(PA, 5) 
+#define VALVE_CONTROL_2_PIN        GPIO_PIN(PA, 7) 
+#define VALVE_WATERING_TIME        (3)                /* seconds */
+#define VALVE_WATERING_TYPE        WATERING_INDIVIDUAL
+
+#define SENSOR_POWER_PIN           GPIO_PIN(PB, 8)
+
+/* TODO: try this from here, rather than in sensebox/include/periph_conf.h */
+//#define ADC_0_REF_DEFAULT                  ADC_REFCTRL_REFSEL_AREFA
+
+/*TODO: try this from here, rather than in ds18/include/ds18_params.h */
+//#define DS18_PARAM_PIN             (GPIO_PIN(PB, 9))
+//#define DS18_PARAM_PULL            (GPIO_OD_PU)
+
+/* light (tsl4531x) and temp/humidity (hdc1000) drivers currently need:
+ *
+ * - USEMODULE += [name] in makefile
+ * - a device descriptor declared here
+ * - initialization by calling the required device_init function
+ * - collection of data by calling the relevant functions
+ * - sending data by calling lora_serialization_write
+ * - handling of data in opensensemap
+ *
+ *   these are the same between the two setups so will remain the same in the
+ *   code for the time being.
+ *
+ *   TODO: make this configurable too
+ */
+
+/* Sensor device descriptors */
 static hdc1000_t hdc1000_dev;
+static tsl4531x_t tsl4531x_dev;
+static ds18_t ds18_dev;
 
 static void rtc_cb(void *arg)
 {
@@ -70,6 +121,7 @@ static void _prepare_next_alarm(void)
 {
     struct tm time;
     rtc_get_time(&time);
+
     /* set initial alarm */
     time.tm_sec += PERIOD;
     mktime(&time);
@@ -79,53 +131,144 @@ static void _prepare_next_alarm(void)
 static void _send_message(void)
 {
     printf("Sending data\n");
+
      /* Reset serialization descriptor */
     lora_serialization_reset(&serialization);
+
      /* Write data to serialization. Replace with your sensors measurements.
      * Keep in mind that the order in which the data is written into the
      * serialization needs to match the decoders order.
      */
-
     
-    /* Air temperature and humidity */
+    /* Air temperature - HDC1000 */
     int16_t res;
     float val;
     hdc1000_read_cached((const hdc1000_t *)&hdc1000_dev, &res, NULL);
     val = (float)res/100;
     lora_serialization_write_temperature(&serialization, val);
+
+    /* Air humidity - HDC1000 */
     hdc1000_read_cached((const hdc1000_t *)&hdc1000_dev, NULL, &res);
     val = (float)res/100;
     lora_serialization_write_humidity(&serialization, val);
 
-    /* Visible light intensity */
-    lora_serialization_write_uint8(&serialization, 213);
+    /* Visible light intensity - tsl4531x */
+    res = tsl4531x_simple_read(&tsl4531x_dev);
+    lora_serialization_write_uint16(&serialization, res);
 
-    /* Soil temperature */
-    lora_serialization_write_temperature(&serialization, 23.4);
+    /* Soil temperature - ds18 */
+    if (ds18_get_temperature(&ds18_dev, &res) < 0) {
+        puts("Soil temp collection failed");
+        res = 0;
+    }
+    val = (float)res/100;
+    lora_serialization_write_temperature(&serialization, val);
 
     /* Soil moisture levels */
-
-    gpio_set(GPIO_PIN(PB, 8));
-    /* Sleep for 10s - the electrolysis effect means that it takes ~5-6s for
+    gpio_set(SENSOR_POWER_PIN);
+    /* Sleep for 10s - the electrolytic effect means that it takes ~5-6s for
      * the reading to settle down
      */
     xtimer_sleep(10);
     int sample;
-    sample = adc_sample(ADC_LINE(0), RES);
-    printf("ADC_LINE(%u): %i\n", 0, sample);
+    sample = adc_sample(MOISTURE_SENSOR_1_PIN, RES);
     lora_serialization_write_uint16(&serialization, sample);
-    sample = adc_sample(ADC_LINE(2), RES);
-    printf("ADC_LINE(%u): %i\n", 2, sample);
-    lora_serialization_write_uint16(&serialization, sample);
-    sample = adc_sample(ADC_LINE(5), RES);
-    printf("ADC_LINE(%u): %i\n", 5, sample);
-    lora_serialization_write_uint16(&serialization, sample);
-    gpio_clear(GPIO_PIN(PB, 8));
+#ifdef VALVE_WATERING_ON
+    if (sample > plant_1_watering_level) {
+        gpio_set(VALVE_CONTROL_1_PIN);
+        xtimer_sleep(VALVE_WATERING_TIME);
+        gpio_clear(VALVE_CONTROL_1_PIN);
+    }
+#endif
 
+    sample = adc_sample(MOISTURE_SENSOR_2_PIN, RES);
+    lora_serialization_write_uint16(&serialization, sample);
+#ifdef VALVE_WATERING_ON
+    if (sample > plant_2_watering_level) {
+        gpio_set(VALVE_CONTROL_2_PIN);
+        xtimer_sleep(VALVE_WATERING_TIME);
+        gpio_clear(VALVE_CONTROL_2_PIN);
+    }
+#endif
+
+    sample = adc_sample(MOISTURE_SENSOR_3_PIN, RES);
+    lora_serialization_write_uint16(&serialization, sample);
+
+    gpio_clear(SENSOR_POWER_PIN);
+
+#ifdef LORA_DATA_SEND_ON
     /* The send call blocks until done */
     semtech_loramac_send(&loramac, serialization.buffer, serialization.cursor);
+
     /* Wait until the send cycle has completed */
     semtech_loramac_recv(&loramac);
+#endif
+
+}
+
+static void _hardware_test(void)
+{
+    /* Test the valves */
+    puts("Testing the valves. Valves should click on and off three times each.");
+    for (int i = 0; i < 3; i++) {
+        gpio_set(VALVE_CONTROL_1_PIN);
+        xtimer_sleep(1);
+        gpio_clear(VALVE_CONTROL_1_PIN);
+        xtimer_sleep(1);
+    }
+    for (int i = 0; i < 3; i++) {
+        gpio_set(VALVE_CONTROL_2_PIN);
+        xtimer_sleep(1);
+        gpio_clear(VALVE_CONTROL_2_PIN);
+        xtimer_sleep(1);
+    }
+
+    /* Test the soil temp, air temp/humidity, and light sensors */
+    puts("Testing the soil temp, air temp/humidity, and light sensors. Check the readings are sensible.");
+    int16_t res;
+    float val;
+    hdc1000_read_cached((const hdc1000_t *)&hdc1000_dev, &res, NULL);
+    val = (float)res/100;
+    printf("Air temperature is %5.2fC.\n", val);
+
+    hdc1000_read_cached((const hdc1000_t *)&hdc1000_dev, NULL, &res);
+    val = (float)res/100;
+    printf("Air humidity is %5.2f%%.\n", val);
+
+    /* Visible light intensity - tsl4531x */
+    res = tsl4531x_simple_read(&tsl4531x_dev);
+    printf("Light intensity is %d\n", res);
+
+    /* Soil temperature - ds18 */
+    if (ds18_get_temperature(&ds18_dev, &res) < 0) {
+        puts("Soil temp collection failed");
+        res = 0;
+    }
+    val = (float)res/100;
+    printf("Soil temperature is %5.2fC.\n", val);
+
+    /* Test the power line for the moisture sensors */
+    puts("Testing moisture sensor power. Light on control board should be blinking on and off.");
+    for (int i = 0; i < 3; i++) {
+        gpio_set(SENSOR_POWER_PIN);
+        xtimer_sleep(1);
+        gpio_clear(SENSOR_POWER_PIN);
+        xtimer_sleep(1);
+    }
+
+    /* Test the moisture sensors */
+    puts("Testing moisture sensors. Check the readings are sensible.");
+    gpio_set(SENSOR_POWER_PIN);
+    /* Sleep for 10s - the electrolytic effect means that it takes ~5-6s for
+     * the reading to settle down
+     */
+    xtimer_sleep(10);
+    int sample = adc_sample(MOISTURE_SENSOR_1_PIN, RES);
+    printf("Plant 1 moisture level is %d.\n", sample);
+    sample = adc_sample(MOISTURE_SENSOR_2_PIN, RES);
+    printf("Plant 2 moisture level is %d.\n", sample);
+    gpio_clear(SENSOR_POWER_PIN);
+
 }
 
 static void *sender(void *arg)
@@ -151,9 +294,10 @@ static void *sender(void *arg)
     puts("Integration with TTN and openSenseMap");
     puts("=====================================");
      /* Convert identifiers and application key */
-    fmt_hex_bytes(deveui, DEVEUI);
-    fmt_hex_bytes(appeui, APPEUI);
+    fmt_hex_bytes(deveui, DEVEUI); fmt_hex_bytes(appeui, APPEUI);
     fmt_hex_bytes(appkey, APPKEY);
+
+#ifdef LORA_DATA_SEND_ON
      /* Initialize the loramac stack */
     semtech_loramac_init(&loramac);
     semtech_loramac_set_deveui(&loramac, deveui);
@@ -171,27 +315,60 @@ static void *sender(void *arg)
         return 1;
     }
     puts("Join procedure succeeded");
+#endif
 
     /* Enable the UART 5V lines */
+    /* TODO: I don't actually need this for Stefan's setup. But a PR should be
+     * raised
+     */
     gpio_init(GPIO_PIN(PB, 2), GPIO_OUT);
     gpio_set(GPIO_PIN(PB, 2));
 
     /* Initialise the sensors */
     hdc1000_init(&hdc1000_dev, &hdc1000_params[0]);
+    tsl4531x_init(&tsl4531x_dev, &tsl4531x_params[0]);
+    ds18_init(&ds18_dev, &ds18_params[0]);
 
     /* Initialise the ADCs */
-    for (unsigned i = 0; i < 5; i = i + 2) {
-        if (adc_init(ADC_LINE(i)) < 0) {
-            printf("ADC %u initialization failed\n", i);
-        }
+    if (adc_init(MOISTURE_SENSOR_1_PIN) < 0) {
+        printf("Moisture sensor 1 ADC initialization failed\n");
+    }
+    if (adc_init(MOISTURE_SENSOR_2_PIN) < 0) {
+        printf("Moisture sensor 2 ADC initialization failed\n");
+    }
+    if (adc_init(MOISTURE_SENSOR_3_PIN) < 0) {
+        printf("Moisture sensor 3 ADC initialization failed\n");
     }
 
     /* Initialise the hygrometer power control line */
-    gpio_init(GPIO_PIN(PB, 8), GPIO_OUT);
+    gpio_init(SENSOR_POWER_PIN, GPIO_OUT);
+
+    /* Initialise the valve control lines */
+    gpio_init(VALVE_CONTROL_1_PIN, GPIO_OUT);
+    gpio_init(VALVE_CONTROL_2_PIN, GPIO_OUT);
+
+#ifdef HARDWARE_TEST_ON
+    _hardware_test();
+#endif
+
+#ifdef VALVE_WATERING_ON
+    /* Set the watering level */
+    gpio_set(SENSOR_POWER_PIN);
+    /* Sleep for 10s - the electrolytic effect means that it takes ~5-6s for
+     * the reading to settle down
+     */
+    xtimer_sleep(10);
+    plant_1_watering_level = adc_sample(MOISTURE_SENSOR_1_PIN, RES);
+    printf("Plant 1 watering level is %d.\n", plant_1_watering_level);
+    plant_2_watering_level = adc_sample(MOISTURE_SENSOR_2_PIN, RES);
+    printf("Plant 2 watering level is %d.\n", plant_2_watering_level);
+    gpio_clear(SENSOR_POWER_PIN);
+#endif
 
     /* start the sender thread */
     sender_pid = thread_create(sender_stack, sizeof(sender_stack),
                                SENDER_PRIO, 0, sender, NULL, "sender");
+
      /* trigger the first send */
     msg_t msg;
     msg_send(&msg, sender_pid);
